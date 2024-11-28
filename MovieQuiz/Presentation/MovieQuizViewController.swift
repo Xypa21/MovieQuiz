deb17fe9fbf12a87fb21ebef0157b8322dcc836c
Xypa21 <odiss123563@gmail.com>
2024-11-28 12:15:45 +0500

    Работа над ошибками

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -9,44 +9,38 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var textLabel: UILabel!
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
-        let currentQuestion = questions[currentQuestionIndex] // 1
-        let givenAnswer = true // 2
-        
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer) // 3
+       let givenAnswer = true
+        let result = showAnswerResult(isCorrect: givenAnswer)
     }
     
     @IBAction private func noButtonClicked(_ sender: UIButton) {
-        let currentQuestion = questions[currentQuestionIndex] // 1
-        let givenAnswer = false // 2
-        
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer) // 3
+       let givenAnswer = false
+        let result = showAnswerResult(isCorrect: givenAnswer)
+    }
+   
+    struct QuizQuestion {
+      // строка с названием фильма,
+      // совпадает с названием картинки афиши фильма в Assets
+      let image: String
+      // строка с вопросом о рейтинге фильма
+      let text: String
+      // булевое значение (true, false), правильный ответ на вопрос
+        var correctAnswer: Bool
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        show(quiz: QuizStepViewModel(image: UIImage(), question: String(), questionNumber: String()))
-        showAnswerResult(isCorrect: Bool())
-        func convert(model: QuizQuestion) -> QuizStepViewModel {
-            let questionStep = QuizStepViewModel( // 1
-                image: UIImage(named: model.image) ?? UIImage(), // 2
-                question: model.text, // 3
-                questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // 4
-            return questionStep
-        }
-        let currentQuestion = questions[currentQuestionIndex]
-        
+    // для состояния "Вопрос показан"
+    struct QuizStepViewModel {
+      let image: UIImage
+      let question: String
+      let questionNumber: String
     }
-    
-    
-    // приватный метод, который меняет цвет рамки
-    // принимает на вход булевое значение и ничего не возвращает
-    private func showAnswerResult(isCorrect: Bool) {
-        imageView.layer.masksToBounds = true // 1
-        imageView.layer.borderWidth = 8 // 2
-        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor // 3
+
+    // для состояния "Результат квиза"
+    struct QuizResultsViewModel {
+      let title: String
+      let text: String
+      let buttonText: String
     }
-    
     
     private let questions: [QuizQuestion] = [
         QuizQuestion(
@@ -90,55 +84,40 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)
     ]
-    
     private var currentQuestionIndex = 0
-    
-    // переменная со счётчиком правильных ответов, начальное значение закономерно 0
     private var correctAnswers = 0
     
-    struct ViewModel {
-      let image: UIImage
-      let question: String
-      let questionNumber: String
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let currentQuestion = questions[currentQuestionIndex]
+        let questionViewModel = convert(model: currentQuestion)
+        show(step: questionViewModel)
     }
     
-    // берём текущий вопрос из массива вопросов по индексу текущего вопроса
-    // для состояния "Вопрос показан"
-    struct QuizStepViewModel {
-        let image: UIImage
-        let question: String
-        let questionNumber: String
+    private func showAnswerResult(isCorrect: Bool) -> Bool {
+        let currentQuestion = questions[currentQuestionIndex]
+        imageView.layer.masksToBounds = true
+            imageView.layer.borderWidth = 8
+            imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        return isCorrect == currentQuestion.correctAnswer
     }
     
-    // для состояния "Результат квиза"
-    struct QuizResultsViewModel {
-        let title: String
-        let text: String
-        let buttonText: String
-    }
-    
-    struct QuizQuestion {
-        // строка с названием фильма,
-        // совпадает с названием картинки афиши фильма в Assets
-        let image: String
-        // строка с вопросом о рейтинге фильма
-        let text: String
-        // булевое значение (true, false), правильный ответ на вопрос
-        let correctAnswer: Bool
+    func show(step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
     }
     
     // приватный метод конвертации, который принимает моковый вопрос и возвращает вью модель для главного экрана
-   
-    
-    
-    // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
-    private func show(quiz step: QuizStepViewModel) {
-      imageView.image = step.image
-      textLabel.text = step.question
-      counterLabel.text = step.questionNumber
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel( // 1
+            image: UIImage(named: model.image) ?? UIImage(), // 2
+            question: model.text, // 3
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // 4
+        return questionStep
     }
     
-   
+       
     
 }
     
