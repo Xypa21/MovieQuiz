357a9c0fbeac7ee8830c4943808231322b21f15b
Xypa21 <odiss123563@gmail.com>
2024-12-01 17:16:33 +0500

    Итог

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -9,37 +9,39 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var textLabel: UILabel!
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
-       let givenAnswer = true
-        let result = showAnswerResult(isCorrect: givenAnswer)
+            let currentQuestion = questions[currentQuestionIndex]
+            let givenAnswer = true
+            let result = showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
     @IBAction private func noButtonClicked(_ sender: UIButton) {
-       let givenAnswer = false
-        let result = showAnswerResult(isCorrect: givenAnswer)
+            let currentQuestion = questions[currentQuestionIndex]
+            let givenAnswer = false
+            let result = showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
    
     struct QuizQuestion {
       // строка с названием фильма,
       // совпадает с названием картинки афиши фильма в Assets
-      let image: String
+            let image: String
       // строка с вопросом о рейтинге фильма
-      let text: String
+            let text: String
       // булевое значение (true, false), правильный ответ на вопрос
-        var correctAnswer: Bool
+            var correctAnswer: Bool
     }
     
     // для состояния "Вопрос показан"
     struct QuizStepViewModel {
-      let image: UIImage
-      let question: String
-      let questionNumber: String
+            let image: UIImage
+            let question: String
+            let questionNumber: String
     }
 
     // для состояния "Результат квиза"
     struct QuizResultsViewModel {
-      let title: String
-      let text: String
-      let buttonText: String
+            let title: String
+            let text: String
+            let buttonText: String
     }
     
     private let questions: [QuizQuestion] = [
@@ -89,35 +91,90 @@ final class MovieQuizViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let currentQuestion = questions[currentQuestionIndex]
-        let questionViewModel = convert(model: currentQuestion)
+            let currentQuestion = questions[currentQuestionIndex]
+            let questionViewModel = convert(model: currentQuestion)
+        
         show(step: questionViewModel)
     }
     
+    // приватный метод, который содержит логику перехода в один из сценариев
+    // метод ничего не принимает и ничего не возвращает
+    private func showNextQuestionOrResults()
+    {
+        if currentQuestionIndex == questions.count - 1 { // 1
+            // идём в состояние "Результат квиза"
+            let text = "Ваш результат: \(correctAnswers)/10" // 1
+                    let viewModel = QuizResultsViewModel( // 2
+                        title: "Этот раунд окончен!",
+                        text: text,
+                        buttonText: "Сыграть ещё раз")
+                     // 3
+            showAlert(result: viewModel)
+        } else { // 2
+            currentQuestionIndex += 1
+            
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(step: viewModel)
+        }
+    }
+
+    
     private func showAnswerResult(isCorrect: Bool) -> Bool {
-        let currentQuestion = questions[currentQuestionIndex]
-        imageView.layer.masksToBounds = true
+            let currentQuestion = questions[currentQuestionIndex]
+        if isCorrect {
+                correctAnswers += 1
+            }
+            imageView.layer.masksToBounds = true
             imageView.layer.borderWidth = 8
             imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        // запускаем задачу через 1 секунду c помощью диспетчера задач
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+           // код, который мы хотим вызвать через 1 секунду
+           self.showNextQuestionOrResults()
+        }
         return isCorrect == currentQuestion.correctAnswer
     }
     
-    func show(step: QuizStepViewModel) {
-        imageView.image = step.image
-        textLabel.text = step.question
-        counterLabel.text = step.questionNumber
+    private func show(step: QuizStepViewModel) {
+            imageView.image = step.image
+            textLabel.text = step.question
+            counterLabel.text = step.questionNumber
+        
+    }
+    
+    private func showAlert(result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: "Этот раунд окончен!",
+            message: "Ваш результат \(correctAnswers)/10",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            // сбрасываем переменную с количеством правильных ответов
+            self.correctAnswers = 0
+            
+            // заново показываем первый вопрос
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(step: viewModel)
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    
     }
     
     // приватный метод конвертации, который принимает моковый вопрос и возвращает вью модель для главного экрана
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        let questionStep = QuizStepViewModel( // 1
+            let questionStep = QuizStepViewModel( // 1
             image: UIImage(named: model.image) ?? UIImage(), // 2
             question: model.text, // 3
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)") // 4
         return questionStep
     }
-    
-       
     
 }
     
